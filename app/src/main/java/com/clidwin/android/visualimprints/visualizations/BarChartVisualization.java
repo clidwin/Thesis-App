94584f5649be50120ebd8fdeb01c7c29661df434
Christina Lidwin <clidwin@gmail.com>
2015-07-29 14:42:54 -0400

    Bar Visualization Clean Up
    Adding grid lines and axis details to the bar chart visualization.

--- app/src/main/java/com/clidwin/android/visualimprints/visualizations/BarChartVisualization.java
+++ app/src/main/java/com/clidwin/android/visualimprints/visualizations/BarChartVisualization.java
@@ -2,7 +2,6 @@ package com.clidwin.android.visualimprints.visualizations;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.NonNull;
@@ -65,10 +64,12 @@ public class BarChartVisualization extends ParentVisualization {
      */
     private void initializePaints() {
         mFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mFillPaint.setColor(Color.LTGRAY);
+        mFillPaint.setColor(getResources().getColor(R.color.grey_300));
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setColor(Color.BLACK);
+        mTextPaint.setColor(getResources().getColor(R.color.TextPrimary));
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.small_text_size));
     }
 
     @Override
@@ -80,12 +81,16 @@ public class BarChartVisualization extends ParentVisualization {
             return;
         }
 
-        // Paint the background.
+        // Paint the background and the grid.
+        mFillPaint.setColor(getResources().getColor(R.color.grey_300));
         canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), mFillPaint);
 
-        float cellWidth = canvas.getWidth() / 24;
-        float cellHeight = canvas.getHeight() / maxBarHeight;
+        float cellWidth = (canvas.getWidth() - 64) / 24;
+        //TODO(clidwin): Make this dynamic (more than 50 shouldn't have as many lines as points
+        float cellHeight = (canvas.getHeight() - 64) / (maxBarHeight+2);
+        drawGrid(canvas, cellWidth, cellHeight);
 
+        // Draw bars
         boolean colorReverse = false;
         for (int i=0; i< barInfo.length; i++, colorReverse = !colorReverse) {
             // Select color.
@@ -96,20 +101,84 @@ public class BarChartVisualization extends ParentVisualization {
 
             // Draw bar
             drawingRect.set(
-                    cellWidth * i,
-                    canvas.getHeight() - (cellHeight * barInfo[i]),
-                    cellWidth * (i + 1),
-                    canvas.getHeight());
+                    cellWidth * i + 64,
+                    (canvas.getHeight() - 72) - (cellHeight * barInfo[i]),
+                    cellWidth * (i + 1) + 64,
+                    canvas.getHeight() - 72);
             canvas.drawRect(drawingRect, mFillPaint);
 
             // Draw bar label
-            canvas.drawText(
-                    "" + barInfo[i],
-                    (float) (cellWidth * (i + 0.5)),
-                    canvas.getHeight() - (cellHeight * barInfo[i]),
-                    mTextPaint
-            );
+            if (barInfo[i] != 0) {
+                canvas.drawText(
+                        "" + barInfo[i],
+                        (float) (cellWidth * (i + 0.5)) + 64,
+                        (canvas.getHeight() - 72) - (cellHeight * barInfo[i]) - 4,
+                        mTextPaint
+                );
+            }
         }
+    }
+
+    /**
+     * Draws the grid lines and labels.
+     * @param canvas The canvas to draw the grid on.
+     * @param cellWidth The height of each cell.
+     * @param cellHeight The width of each cell.
+     */
+    private void drawGrid(Canvas canvas, float cellWidth, float cellHeight) {
+
+        mFillPaint.setColor(getResources().getColor(R.color.grey_400));
+        // Draw the vertical quantity divisions.
+        for (int i=0; i<maxBarHeight + 2; i++) {
+            float verticalDraw = ((maxBarHeight + 2) - i)*cellHeight;
+            Log.e(TAG, "VerticalDraw: " + i);
+
+            if (i%5 == 0) {
+                mFillPaint.setColor(getResources().getColor(R.color.grey_500));
+                canvas.drawLine(56, verticalDraw, canvas.getWidth(), verticalDraw, mFillPaint);
+                mFillPaint.setColor(getResources().getColor(R.color.grey_400));
+
+                canvas.drawText("" + i, 40, verticalDraw, mTextPaint);
+            } else {
+                canvas.drawLine(64, verticalDraw, canvas.getWidth(), verticalDraw, mFillPaint);
+            }
+        }
+
+        // Draw the timestamp dividing lines
+        //TODO(clidwin): Make this dynamic for custom timeframes
+        for (int j=0; j<=24; j++) {
+            if (j%5 == 0) {
+                mFillPaint.setColor(getResources().getColor(R.color.grey_500));
+                canvas.drawLine(
+                        j * cellWidth + 64, 0,
+                        j * cellWidth + 64, canvas.getHeight() - 56,
+                        mFillPaint);
+                mFillPaint.setColor(getResources().getColor(R.color.grey_400));
+
+                canvas.drawText("" + j, j * cellWidth + 64, canvas.getHeight() - 40, mTextPaint);
+            } else {
+                canvas.drawLine(
+                        j*cellWidth + 64, 0, j*cellWidth + 64, canvas.getHeight() - 64, mFillPaint);
+            }
+        }
+
+        // Draw axis labels.
+        mTextPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.default_text_size));
+        canvas.drawText(
+                "Recorded Hour",
+                canvas.getWidth() / 2,
+                canvas.getHeight() - 16,
+                mTextPaint);
+
+        canvas.save();
+        canvas.rotate(270, canvas.getWidth()/2, canvas.getHeight()/2);
+        canvas.drawText(
+                "Number of Data Points",
+                canvas.getWidth()/2,
+                canvas.getHeight()/4 - 32,
+                mTextPaint);
+        canvas.restore();
+        mTextPaint.setTextSize(getResources().getDimensionPixelSize(R.dimen.small_text_size));
     }
 
     @Override
